7e75c84c3dbe03589200e85df6fa184c20ee2276
Yoga Agustiansyah <yogaagustiansyah01@gmail.com>
2023-06-01 20:22:37 +0700

    queue module done

--- src/modules/queue.cpp
+++ src/modules/queue.cpp
@@ -1,1 +1,39 @@
-// abc
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+class Queue {
+private:
+    deque<string> queue;
+
+public:
+    void enqueue(string data) {
+        queue.push_back(data);
+    }
+
+    string dequeue() {
+        if (queue.empty()) {
+            cout << "Queue kosong" << endl;
+            return "";
+        } else {
+        string front = queue.front();
+        queue.pop_front();
+        return front;
+        }   
+    }
+
+    void clear_queue() {
+        queue.clear();
+    }
+
+    void show_queue() {
+        if (queue.empty()) {
+            cout << "Queue kosong" << endl;
+        } else {
+            for (int i = 0; i < queue.size(); ++i) {
+                cout << i + 1 << ". " << queue[i] << endl;
+            }
+        }
+    }
+};
